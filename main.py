1c24919ecdd3be50d1358bae72a3d05b48784fcd
Amartyanov1974 <randomquake@yandex.ru>
2023-12-02 14:57:37 +0400

    добавил запись в файл, параметры командной строки

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import asyncio
+import aiofiles
+import argparse
+from datetime import datetime
+
+
+def read_args():
+    parser = argparse.ArgumentParser(description='Утилита для подключения к чату ')
+    parser.add_argument('-host', type=str, default='minechat.dvmn.org', help='Имя хоста (default=minechat.dvmn.org)')
+    parser.add_argument('-port', type=int, default=5000, help='Номер порта (default=5000)')
+    parser.add_argument('-logfile', type=str, default='logfile.txt', help='Имя файла логов (default=logfile.txt)')
+    args = parser.parse_args()
+    return args
+
 
 async def tcp_echo_client():
+    args = read_args()
+    host, port, logfile = args.host, args.port, args.logfile
     try:
-        reader, writer = await asyncio.open_connection(
-            'minechat.dvmn.org', 5000)
-        while True:
-            data = await reader.readline()
-            text = f'{data.decode()!r}'[1:-3]
-            text.rstrip()
-            print(text)
+        reader, writer = await asyncio.open_connection(host, port)
+        async with aiofiles.open(logfile, 'a') as chat_log:
+            while True:
+                log_time = datetime.now()
+                log_time = log_time.strftime('%Y-%m-%d %H:%M:%S')
+                data = await reader.readline()
+                text = f'{data.decode()!r}'[1:-3]
+                text.rstrip()
+                text = f'{log_time} - {text}'
+                print( text)
+                await chat_log.write(f'{text}\n')
     finally:
         print('Close the connection')
         writer.close()
